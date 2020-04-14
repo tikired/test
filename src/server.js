ac033841d78d5f6720e947f9b0e6e0cbd95845d8
tikired <egdoglia@gmail.com>
2020-04-14 18:19:28 -0400

    test

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/test'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/test/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
